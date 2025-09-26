c5dba84b2f469f43f546919a6c7cc5a311effeb2
nicolee <nicolemathiasdasilva@gmail.com>
2025-09-26 20:42:54 -0300

    FINALIZAR

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.WriteLine("digite seu primeiro nome:");
+string nome = Console.ReadLine();
+Console.WriteLine("digite seu sobrenome:");
+string sobrenome = Console.ReadLine();
+Console.WriteLine($"nome completo:{nome},{sobrenome}");
+Console.WriteLine($"nome catalogo: {sobrenome.ToUpper()} ,{nome} ");
