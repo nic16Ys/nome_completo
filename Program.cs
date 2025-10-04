6731457bd52d8eae06d92d046fb7b17342855d6e
nic16Ys <nicolemathiasdasilva@gmail.com>
2025-10-03 21:51:52 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,5 +3,5 @@ Console.WriteLine("digite seu primeiro nome:");
 string nome = Console.ReadLine();
 Console.WriteLine("digite seu sobrenome:");
 string sobrenome = Console.ReadLine();
-Console.WriteLine($"nome completo:{nome},{sobrenome}");
-Console.WriteLine($"nome catalogo: {sobrenome.ToUpper()} ,{nome} ");
+Console.WriteLine($"nome completo:{nome} {sobrenome}");
+Console.WriteLine($"nome catalogo: {sobrenome.ToUpper()} , {nome} ");
